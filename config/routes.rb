207bd56fe72ac330da213a6c8976d7691b45c8ba
EC2 Default User <110474164+Jupiter200108@users.noreply.github.com>
2022-08-08 03:01:08 +0000

    Commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   get 'books' => 'books#index'
   post 'books/' => 'books#create' 
-  get 'books/' => 'books#show'
-  get 'books/edit' => 'books#edit'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
   get root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
